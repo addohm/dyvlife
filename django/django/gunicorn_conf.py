d4a1c2db112991d3a0dc6745f55670b82a7c5d15
Adam S. Leven <addohm@noreply.users.github.com>
2025-07-31 23:19:10 +0800

    Duped django directory.  Not sure where it came from.

--- django/django/gunicorn_conf.py
+++
@@ -1,8 +0,0 @@
-# /home/addohm/django/project/gunicorn_conf.py
-# screen gunicorn -c gunicorn_conf.py project.wsgi:application
-bind = '0.0.0.0:8000'
-worker_class = 'sync'
-loglevel = 'debug'
-accesslog = '/var/log/gunicorn/django_access_log'
-acceslogformat ="%(h)s %(l)s %(u)s %(t)s %(r)s %(s)s %(b)s %(f)s %(a)s"
-errorlog =  '/var/log/gunicorn/django_error_log'
